081b01f1eed6ea3a79311ae0c9d42975a5d2369b
DKaramfilov <mpc@gbg.bg>
2013-02-14 12:34:47 +0200

    Some new Implementations

---
+++ BinarySearch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Tasc11_BinarySearch
+{
+    static void Main()
+    {
+        Console.Write("Enter key to search:");
+        int key = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter array length: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write("Element{0}=", i + 1);
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+        Array.Sort(arr);
+
+        int result = BinarySearch(key, arr);
+        if (result > 0) Console.WriteLine("index of key{0} is {1}", key, result);
+        else Console.WriteLine("Not Found");
+
+
+    }
+    public static int BinarySearch(int key, int[] a)
+    {
+        int lo = 0;
+        int hi = a.Length - 1;
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+
+    }
+}
+
---
+++ MergeSort.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class MergeSortRecursive
+{
+    static Random rand = new Random();
+    static void Main()
+    {
+        int n = rand.Next(20, 30);
+        int[] nums = new int[n];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = rand.Next(100);
+        }
+
+        MergeSort(nums, 0, nums.Length - 1);
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Console.WriteLine(nums[i]);
+        }
+    }
+    static public void MergeSort(int[] s, int low, int high)
+    {
+        int middle;
+
+        if (low < high)
+        {
+            middle = (low + high) / 2;
+            MergeSort(s, low, middle);
+            MergeSort(s, middle + 1, high);
+            Merge(s, low, middle + 1, high);
+        }
+    }
+    static public void Merge(int[] s, int low, int middle, int high)
+    {
+        int leftEnd, numElelments, tempPos;
+        int[] temp = new int[s.Length];
+
+        leftEnd = middle - 1;
+        tempPos = low;
+        numElelments = high - low + 1;
+
+        while ((low <= leftEnd) && (middle <= high))
+        {
+            if (s[low] <= s[middle])
+            {
+                temp[tempPos] = s[low];
+                tempPos++;
+                low++;
+            }
+            else
+            {
+                temp[tempPos] = s[middle];
+                tempPos++;
+                middle++;
+            }
+        }
+        while (low <= leftEnd)
+        {
+            temp[tempPos] = s[low];
+            tempPos++;
+            low++;
+        }
+        while (middle <= high)
+        {
+            temp[tempPos] = s[middle];
+            tempPos++;
+            middle++;
+        }
+        for (int i = 0; i < numElelments; i++)
+        {
+            s[high] = temp[high];
+            high--;
+        }
+    }
+}
+
+
+
